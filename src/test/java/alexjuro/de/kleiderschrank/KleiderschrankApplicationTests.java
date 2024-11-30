aff1cb7f9efc9c90431dda4280d35c5386dd426d
Alexander Lesnjak <alexander.lesnjak@gmail.com>
2024-11-30 17:11:55 +0100

    initial commit

---
+++ src/test/java/alexjuro/de/kleiderschrank/KleiderschrankApplicationTests.java
@@ -0,0 +1,13 @@
+package alexjuro.de.kleiderschrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KleiderschrankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
